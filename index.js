9a0946327d1c1f3e6dfb2cbc06cca66df6461d49
Isaac Marotte <imarotte@gmail.com>
2021-04-23 05:31:50 -0700

    update documentation

--- index.js
+++ index.js
@@ -7,29 +7,74 @@ const schedule = require('node-schedule');
 module.exports = {
 
   /**
-   * @param {object} ruleObj
-   * @param {?number|number[]} [ruleObj.second = 0] - 0-59 null makes it tick every second
-   * @param {?number|number[]} [ruleObj.minute] - 0-59 must be explicitly set to 0 if hour is set
-   *                                              otherwise will assume every minute
-   * @param {?number|number[]} [ruleObj.hour] 0-23
-   * @param {?number|number[]} [ruleObj.date] 1-31
-   * @param {?number|number[]} [ruleObj.month] 0-11
-   * @param {?number|number[]} [ruleObj.year]
-   * @param {?number|number[]} [ruleObj.dayOfWeek] (0-6) Starting with Sunday
+   * @param {RecurrenceRule} recurrenceRule
    * @param {function} callback
-   * @return {Job|*}
+   * @return {SchedulerJob}
    */
-  recurrenceByTime(ruleObj, callback) {
+  recurrenceByTime(recurrenceRule, callback) {
     const rule = new schedule.RecurrenceRule();
 
-    rule.second = ruleObj.second === undefined ? 0 : ruleObj.second;
-    if (ruleObj.minute !== undefined) { rule.minute = ruleObj.minute; }
-    if (ruleObj.hour !== undefined) { rule.hour = ruleObj.hour; }
-    if (ruleObj.date !== undefined) { rule.date = ruleObj.date; }
-    if (ruleObj.month !== undefined) { rule.month = ruleObj.month; }
-    if (ruleObj.year !== undefined) { rule.year = ruleObj.year; }
-    if (ruleObj.dayOfWeek !== undefined) { rule.dayOfWeek = ruleObj.dayOfWeek; }
+    rule.second = recurrenceRule.second === undefined ? 0 : recurrenceRule.second;
+    if (recurrenceRule.minute !== undefined) { rule.minute = recurrenceRule.minute; }
+    if (recurrenceRule.hour !== undefined) { rule.hour = recurrenceRule.hour; }
+    if (recurrenceRule.date !== undefined) { rule.date = recurrenceRule.date; }
+    if (recurrenceRule.month !== undefined) { rule.month = recurrenceRule.month; }
+    if (recurrenceRule.year !== undefined) { rule.year = recurrenceRule.year; }
+    if (recurrenceRule.dayOfWeek !== undefined) { rule.dayOfWeek = recurrenceRule.dayOfWeek; }
 
     return schedule.scheduleJob(rule, callback);
   }
 };
+
+/**
+ * @typedef RecurrenceRule
+ * @property {?number|number[]} [ruleObj.second = 0] - 0-59 null makes it tick every second
+ * @property {?number|number[]} [ruleObj.minute] - 0-59 must be explicitly set to 0 if hour is set
+ *                                              otherwise will assume every minute
+ * @property {?number|number[]} [ruleObj.hour] 0-23
+ * @property {?number|number[]} [ruleObj.date] 1-31
+ * @property {?number|number[]} [ruleObj.month] 0-11
+ * @property {?number|number[]} [ruleObj.year]
+ * @property {?number|number[]} [ruleObj.dayOfWeek] (0-6) Starting with Sunday
+ */
+
+
+/**
+ * @class SchedulerJob
+ *
+ */
+
+/**
+ * All planned invocations will be canceled.When you set the parameter reschedule to
+ * true then the Job is newly scheduled afterwards.
+ * @function cancel
+ * @memberOf SchedulerJob#
+ * @see [node-schedule/cancel]{@link https://github.com/node-schedule/node-schedule#jobcancelreschedule}
+ */
+
+/**
+ * This method invalidates the next planned invocation or the job. When you set the parameter
+ * reschedule to true then the Job is newly scheduled afterwards.
+ * @callback cancelNext
+ * @memberOf SchedulerJob#
+ * @see [node-schedule/cancelNext]{@link https://github.com/node-schedule/node-schedule#jobcancelnextreschedule}
+ * @param reschedule
+ */
+
+
+/**
+ * This method cancels all pending invocation and registers the Job completely new again using
+ * the given specification. Return true/false on success/failure.
+ * @function reschedule
+ * @memberOf SchedulerJob#
+ * @see [node-schedule/reschedule]{@link https://github.com/node-schedule/node-schedule#jobreschedulespec}
+ * @param spec
+ */
+
+/**
+ * This method returns a Date object for the planned next Invocation for this Job.
+ * If no invocation is planned the method returns null.
+ * @function nextInvocation
+ * @memberOf SchedulerJob#
+ * @see [node-schedule/nextInvocation]{@link https://github.com/node-schedule/node-schedule#jobnextinvocation}
+ */
